2b0afddbc4964777fb333176dc24f614aa4fa850
leucinemymind <emilyy2009x@gmail.com>
2026-03-17 11:25:24 -0700

    more cleanup

--- rockfishdata_template.R
+++
@@ -1,9 +0,0 @@
-### rockfish data ###
-
-# load in csv
-library(tidyverse)
-library(readr)
-emery_fishlog <- read_csv("Downloads/Emeryville_fish_log - Sheet1.csv")
-
-ggplot(emery_fishlog, aes(x = emery_fishlog$Location, y = emery_fishlog$MP_Stomach,
-                      )) + geom_boxplot()
